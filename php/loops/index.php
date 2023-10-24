37bdba10ce3e1a4c56e93789124a14ba90b87a9d
Oskar Szymański <oskar.szymanski1608@gmail.com>
2023-10-24 19:15:21 +0200

    Loops php

--- php/loops/index.php
+++ php/loops/index.php
@@ -8,17 +8,23 @@
 // If the number is a multiple of 4, write Fire instead of the number
 // If the number is a multiple of 7, write Boom instead of the number
 // If the number is a multiple of 10, repeat it 10 times in the same line in red
-for ($i=100; $i >=0 ; $i--) { 
-    if ($i%4 == 0) {
-        echo "Fire <br>";
-    }
-    else if ($i%7 == 0) {
-        echo "Boom <br>";
-    }
-    else if ($i%10 == 0) {
-        echo '<p style="color:red;margin:0;">'.str_repeat($i,10)."</p>";
-    }
-    else{
+for ($i=100; $i >=0 ; $i--){
+    if ($i%4 != 0 && $i%7 != 0 && $i%10 != 0) {
         echo $i."<br>";
+        continue;
     }
+    $p ='';
+    if ($i%4 == 0) {
+        $p .="Fire";
+    }
+    if ($i%7 == 0) {
+        $p .="Boom";
+    }
+    if ($i%10 == 0) {
+        $p ='<p style="color:red;margin:0;">'.str_repeat($p,10)."</p>";
+        echo $p;
+        continue;
+    }
+    echo $p."</br>";
+    
 }
